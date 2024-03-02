a238d3fc0d7db2dec7997b366262147c28ad1764
Premsaisiddana <psiddana@cbu.edu>
2024-03-02 13:04:18 -0600

    ex_5_3.py
    Completed

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -1,19 +1,26 @@
-"""ex_5_3.py
+""" ex_5_3.py
 This module contains an entry point that:
-
-- creates a CLI that accepts an input file of data to be processed
+- creates a CLi that accepts an input file of data to be processed
 - shifts and scales the data to a mean of 0 and a standard deviation 1
 - writes the file to the output file argument
 """
 import numpy as np
 from argparse import ArgumentParser
+try:
+    from src.util import get_repository_root
+except ImportError:
+    from util import get_repository_root
 
-if _name_ == "_main_":
-    parser = ArgumentParser(description="This program applies a standard scale transform to the data in infile and writes it to outfile.")
-    parser.add_argument("infile", help="Input filename for the data file that needs to be processed.")
-    parser.add_argument("outfile", help="Output filename.")
+if __name__ == "__main__":
+    import os
+    parser = ArgumentParser(description='This programme writes the data from the input file to the output file after applying a standard scale transform..')
+    parser.add_argument('infile',help='provide input file path',nargs='?')
+    parser.add_argument('outfile',help='provide output file path',nargs='?')
     args = parser.parse_args()
-    data = np.loadtxt(args.infile, delimiter=',')
-    data_mean_zero = data - np.mean(data)
-    processed = data_mean_zero / np.std(data_mean_zero)
-    np.savetxt(args.outfile, processed,delimiter=',')
+    input_data = np.loadtxt(args.infile)
+    normalized = (input_data - input_data.mean(axis=0)) / input_data.std(axis=0)
+    processed = normalized
+    root_dir = get_repository_root()
+    os.makedirs(root_dir / "outputs", exist_ok=True)
+    np.savetxt(args.outfile, processed, fmt='%.2e')
+
